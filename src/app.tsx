0eede53d4e6d04d5dd10f00cc5410664563f4106
Ardian Shala <ardian.sh@outlook.de>
2020-09-21 08:03:20 +0200

    updated packages added editorconfig updated webpack.config

--- src/app.tsx
+++ src/app.tsx
@@ -3,23 +3,23 @@ import './style.scss';
 import { ButtonCounter } from './app/button-counter';
 
 export class App extends Component<{}, {}> {
-    onChildClicked(e: number) {
-        console.warn('callback from parent triggered', e);
-    }
+	onChildClicked(e: number) {
+		console.warn('callback from parent triggered', e);
+	}
 
-    render() {
-        return (
-            <div class="container pt-2">
-                <h1>Preact Typescript Starter Template</h1>
+	render() {
+		return (
+			<div className='container pt-2'>
+				<h1>Preact Typescript Starter Template</h1>
 
-                <p>
-                    Simple Preact Typescript TSX Template with a custom ButtonCounter Component.
+				<p>
+					Simple Preact Typescript TSX Template with a custom ButtonCounter Component.
                 </p>
 
-                <ButtonCounter name="Preact TSX" onClicked={(e) => this.onChildClicked(e)} />
-            </div>
-        )
-    }
+				<ButtonCounter name='Preact TSX' onClicked={(e) => this.onChildClicked(e)} />
+			</div>
+		);
+	}
 }
 
 render(<App />, document.body);
